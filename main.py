d8c1aef70f385bb9144bf8acc5feda308fa154a2
kanezikii <peng320829@gmail.com>
2026-07-16 09:15:21 +0800

    Update main.py

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ except ImportError:
 # 配置区域
 # ==============================================================================
 RENEW_URLS = [
-    "https://host2play.gratis/server/renew?i=6666666-6666-6666-6666-66666666666",
+    "https://host2play.gratis/server/renew?i=86d385fd-2ed6-4467-babb-80fad3119508",
     # 添加更多链接
 ]
 
